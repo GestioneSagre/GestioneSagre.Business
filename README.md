7a2a70d5bb4ed6d3d2528cbf92f44f5bb179f067
Angelo Pirola <angelo9871@gmail.com>
2022-07-11 13:15:44 +0200

    Migration from old project

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# GestioneSagre.Business
-DBContext
+# GestioneSagre Business
---
+++ src/GestioneSagre.Business/Services/GestioneSagreDbContext.cs
@@ -0,0 +1,76 @@
+using GestioneSagre.Infrastructure.Entities;
+using GestioneSagre.Infrastructure.Enums;
+using GestioneSagre.Infrastructure.ValueObjects;
+using Microsoft.EntityFrameworkCore;
+
+namespace GestioneSagre.Business.Services;
+
+public partial class GestioneSagreDbContext : DbContext
+{
+    public GestioneSagreDbContext(DbContextOptions<GestioneSagreDbContext> options) : base(options)
+    {
+    }
+
+    public virtual DbSet<FestaEntity> Feste { get; set; }
+    public virtual DbSet<IntestazioneEntity> Intestazioni { get; set; }
+    public virtual DbSet<VersioneEntity> Versioni { get; set; }
+    public virtual DbSet<CategoriaEntity> Categorie { get; set; }
+    public virtual DbSet<ProdottoEntity> Prodotti { get; set; }
+
+    protected override void ConfigureConventions(ModelConfigurationBuilder configurationBuilder)
+    {
+        configurationBuilder.Properties<Currency>().HaveConversion<string>();
+        configurationBuilder.Properties<decimal>().HaveConversion<float>();
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        //Mapping per gli owned types
+        modelBuilder.Owned<Money>();
+
+        //Tabella FESTA
+        modelBuilder.Entity<FestaEntity>(entity =>
+        {
+            entity.ToTable("Festa");
+            entity.HasKey(festa => festa.Id);
+
+            entity.HasMany(festa => festa.Intestazioni)
+                   .WithOne(intestazione => intestazione.Festa)
+                   .HasForeignKey(intestazione => intestazione.FestaId);
+        });
+
+        //Tabella INTESTAZIONE
+        modelBuilder.Entity<IntestazioneEntity>(entity =>
+        {
+            entity.ToTable("Intestazione");
+            entity.HasKey(intestazione => intestazione.Id);
+        });
+
+        //Tabella VERSIONE
+        modelBuilder.Entity<VersioneEntity>(entity =>
+        {
+            entity.ToTable("Versione");
+            entity.HasKey(versione => versione.Id);
+        });
+
+        //Tabella CATEGORIE
+        modelBuilder.Entity<CategoriaEntity>(entity =>
+        {
+            entity.ToTable("Categoria");
+            entity.HasKey(categoria => categoria.Id);
+
+            entity.HasMany(categoria => categoria.Prodotti)
+                   .WithOne(prodotto => prodotto.Categoria)
+                   .HasForeignKey(prodotto => prodotto.CategoriaId);
+        });
+
+        //Tabella PRODOTTO
+        modelBuilder.Entity<ProdottoEntity>(entity =>
+        {
+            entity.ToTable("Prodotto");
+            entity.HasKey(prodotto => prodotto.Id);
+        });
+    }
+}
